9079a07eaa625aaaea872b569345a82f298f1352
Festive <27864684+mash-reza@users.noreply.github.com>
2019-10-01 10:09:26 +0330

    init

---
+++ app/src/main/java/com/example/cliforcast/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cliforcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
