b4a11bc871b9d0358fedd94982826c45e014be2a
Festive <27864684+mash-reza@users.noreply.github.com>
2019-10-02 13:19:30 +0330

    location request

---
+++ app/src/main/java/com/example/cliforcast/Util/Constants.java
@@ -0,0 +1,12 @@
+package com.example.cliforcast.Util;
+
+public class Constants {
+    public static final int LOCATION_REQUSET_CODE = 100;
+    public static final int WIFI_STATE_REQUSET_CODE = 200;
+    public static final int GOOGLE_PLAY_SERVICE_MISSING_REQUEST_CODE = 200;
+    public static final int GOOGLE_PLAY_SERVICE_VERSION_UPDATE_REQUIRED = 300;
+    public static final int GOOGLE_PLAY_SERVICE_DISABLED = 400;
+    public static final String PREFERENCES = "Pref";
+    public static final String LOCATION_PREFERENCES = "locationPref";
+    public static final String WIFI_PREFERENCES = "wifiPref";
+}
--- app/src/main/java/com/example/cliforcast/view/CurrentWeather.java
+++ app/src/main/java/com/example/cliforcast/view/CurrentWeather.java
@@ -1,16 +1,31 @@
 package com.example.cliforcast.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.cliforcast.R;
+import com.example.cliforcast.Util.Constants;
 import com.example.cliforcast.database.RoomHelper;
 import com.example.cliforcast.network.Weather;
 import com.example.cliforcast.network.RetrofitClientInstance;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.Date;
 
@@ -20,12 +35,19 @@ import retrofit2.Response;
 
 public class CurrentWeather extends AppCompatActivity {
     private static final String TAG = "CurrentWeather";
+    private SharedPreferences preferences;
+
+    //GoogleApiClient client;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_weather);
 
+        preferences = getSharedPreferences(Constants.LOCATION_PREFERENCES, MODE_PRIVATE);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        checkPermission();
 
         RetrofitClientInstance.getINSTANCE().getWeather("Las vegas,us").enqueue(new Callback<Weather>() {
             @Override
@@ -112,9 +134,82 @@ public class CurrentWeather extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Weather> call, Throwable t) {
-                Log.e(TAG, "onFailure: ", t);
+                Log.e(TAG, "onFailure: " + t.getMessage());
                 //System.err.println(t);
             }
         });
+
+    }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, Constants.LOCATION_REQUSET_CODE);
+        }
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_WIFI_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_WIFI_STATE}, Constants.WIFI_STATE_REQUSET_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case Constants.LOCATION_REQUSET_CODE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    preferences.edit().putBoolean(Constants.LOCATION_PREFERENCES, true).apply();
+                } else {
+                    new AlertDialog.Builder(this)
+                            .setMessage(R.string.location_permission_rejected_messege_alert_dialog)
+                            .setPositiveButton(R.string.i_agree, (dialog, which) -> {
+                                checkPermission();
+                            })
+                            .setNegativeButton(R.string.i_disagree, (dialog, which) -> {
+                            })
+                            .create()
+                            .show();
+                }
+                break;
+            case Constants.WIFI_STATE_REQUSET_CODE:
+                if ((grantResults.length > 0) && (grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                    preferences.edit().putBoolean(Constants.WIFI_PREFERENCES, true).apply();
+                } else {
+                    new AlertDialog.Builder(this)
+                            .setMessage(R.string.wifit_permission_rejected_messege_alert_dialog)
+                            .setPositiveButton(R.string.i_agree, (dialog, which) -> {
+                                checkPermission();
+                            })
+                            .setNegativeButton(R.string.i_disagree, (dialog, which) -> {
+                            })
+                            .create()
+                            .show();
+                }
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(5000);
+        request.setFastestInterval(1000);
+        fusedLocationProviderClient.requestLocationUpdates(request, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult == null) {
+                    Toast.makeText(CurrentWeather.this, "null location result", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(CurrentWeather.this, locationResult.getLastLocation().getLatitude() + " - " +
+                        locationResult.getLastLocation().getLongitude(), Toast.LENGTH_SHORT).show();
+            }
+        }, Looper.getMainLooper());
     }
 }
